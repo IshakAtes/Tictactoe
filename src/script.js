6a830e9bdeeb9aa982eab2026304db65d88e24a4
IshakAtes <ishakfeuer@gmail.com>
2022-07-25 01:17:31 +0200

    added function check for Winner and Show/hide Player headline

--- src/script.js
+++ src/script.js
@@ -6,13 +6,18 @@ let currentShape = 'cross';
 function fillShape(id){
     if(currentShape == 'cross'){
         currentShape = 'circle';
+        document.getElementById('player-1').classList.add('inactivePlayer')
+        document.getElementById('player-2').classList.remove('inactivePlayer')
     } else{
         currentShape = 'cross';
+        document.getElementById('player-1').classList.remove('inactivePlayer')
+        document.getElementById('player-2').classList.add('inactivePlayer')
     }
 
     fields[id] = currentShape;
     console.log(fields);
     draw();
+    checkForWin();
 }
 
 
@@ -26,5 +31,46 @@ function draw(){
             document.getElementById(`cross-${i}`).classList.remove('d-none');
         }
         
+    }
+}
+
+
+function checkForWin(){
+    let winner;
+
+    if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]){
+        winner = fields[0]
+    }
+
+    if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]){
+        winner = fields[3]
+    }
+
+    if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
+        winner = fields[6]
+    }
+
+    if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]){
+        winner = fields[0]
+    }
+
+    if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]){
+        winner = fields[1]
+    }
+
+    if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]){
+        winner = fields[2]
+    }
+
+    if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]){
+        winner = fields[0]
+    }
+
+    if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]){
+        winner = fields[2]
+    }
+
+    if(winner){
+        console.log('Gewonnen', winner);
     }
 }
