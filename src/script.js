e96e6ed31166f587d46fcd9d98d8e188154c65d4
IshakAtes <ishakfeuer@gmail.com>
2022-07-25 05:05:05 +0200

    added GameOver Animation

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 let fields = [];
-
+let gameOver = false;
 let currentShape = 'cross';
 
 
 function fillShape(id){
-    if(!fields[id]){
+    if(!fields[id] && !gameOver){
         if(currentShape == 'cross'){
             currentShape = 'circle';
             document.getElementById('player-1').classList.add('inactivePlayer')
@@ -89,5 +89,9 @@ function checkForWin(){
 
     if(winner){
         console.log('Gewonnen', winner);
+        gameOver = true;
+        setTimeout(function () {
+            document.getElementById('gameOver').classList.remove('d-none');
+        }, 2000);
     }
 }
