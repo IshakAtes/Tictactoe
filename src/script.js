3342cf89f8fb0ada3ec065f84833e556c2e9ed82
IshakAtes <ishakfeuer@gmail.com>
2022-07-25 04:40:09 +0200

    added animated Winning Line

--- src/script.js
+++ src/script.js
@@ -39,36 +39,52 @@ function draw(){
 function checkForWin(){
     let winner;
 
+    // FIRST ROW
     if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]){
-        winner = fields[0]
+        winner = fields[0];
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
     }
 
+    // SECOND ROW
     if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]){
-        winner = fields[3]
+        winner = fields[3];
+        document.getElementById('line-2').style.transform = 'scaleX(1)';
     }
 
+    // THIRD ROW
     if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
-        winner = fields[6]
+        winner = fields[6];
+        document.getElementById('line-3').style.transform = 'scaleX(1)';
     }
 
+    // FIRST COLUMN
     if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]){
-        winner = fields[0]
+        winner = fields[0];
+        document.getElementById('line-4').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
+    // SECOND COLUMN
     if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]){
-        winner = fields[1]
+        winner = fields[1];
+        document.getElementById('line-5').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
+    // THIRD COLUMN
     if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]){
-        winner = fields[2]
+        winner = fields[2];
+        document.getElementById('line-6').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
+    // DIAGONAL
     if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]){
-        winner = fields[0]
+        winner = fields[0];
+        document.getElementById('line-7').style.transform = 'scaleX(1) rotate(45deg)';
     }
 
+    // DIAGONAL
     if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]){
-        winner = fields[2]
+        winner = fields[2];
+        document.getElementById('line-8').style.transform = 'scaleX(1) rotate(-45deg)';
     }
 
     if(winner){
